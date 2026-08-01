e0b785803776ccd905a39da04c2f3d2affafc0de
Miguelma1969 <marin.desertaz@gmail.com>
2026-08-01 10:48:30 -0500

    Delete app directory

--- app/static/sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='facefind-v1';
-const ASSETS=['/','/static/styles.css','/static/app.js','/manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
-});
